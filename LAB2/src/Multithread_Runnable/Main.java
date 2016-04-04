3455d3a03926f981afd595ed82da82bb0c5cdc14
spiking <a.thuvesen@gmail.com>
2016-04-04 19:15:55 +0200

    LAB2 done, more or less

---
+++ LAB2/src/Multithread_Runnable/Main.java
@@ -0,0 +1,15 @@
+package Multithread_Runnable;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		String url = "http://cs.lth.se/eda095/foerelaesningar/?no_cache=1";
+		Downloader goAndGetThemFiles = new Downloader(url);
+		goAndGetThemFiles.download();
+		
+		
+	}
+
+}
---
+++ LAB2/src/Multithread_Threads/Main.java
@@ -0,0 +1,15 @@
+package Multithread_Threads;
+
+import Multithread_Threads.Downloader;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String url = "http://cs.lth.se/eda095/foerelaesningar/?no_cache=1";
+		Downloader goAndGetThemFiles = new Downloader(url);
+		goAndGetThemFiles.download();
+
+	}
+
+}
